9bff899e8b6834477f26b742f3bac66984dbaa2d
yuanjing <yuanjing@cdzgph.com>
2020-05-28 14:30:24 +0800

    [new]新建项目

---
+++ src/main/java/com/yuanj/sharemusic/SharemusicApplication.java
@@ -0,0 +1,13 @@
+package com.yuanj.sharemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharemusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharemusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuanj/sharemusic/SharemusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanj.sharemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
